2e402d8280f18299ef0021871b7fc3b9404d7cf7
Gabriele Lodde <gabrilodde@gmail.com>
2023-04-01 11:13:32 +0200

    aggiunta funzione setInterval

--- countdown/script.js
+++ countdown/script.js
@@ -7,10 +7,10 @@ let ss = document.querySelector("#ss")
 // il numero dei millisecondi dalla data del 1/1/1970
 
 setInterval(() => {
-    let dt1 = new Date("2023-06-10").getTime()
-    let dt2 = new Date().getTime()
+    let dt_1 = new Date("2023-06-10").getTime()
+    let dt_2 = new Date().getTime()
 
-    let diff_sec = parseInt((dt1-dt2)/1000)
+    let diff_sec = parseInt((dt_1-dt_2)/1000)
 
     gg.innerHTML = parseInt(diff_sec / (60*60*24))
     diff_sec -= parseInt((gg.innerHTML) * (60*60*24))
